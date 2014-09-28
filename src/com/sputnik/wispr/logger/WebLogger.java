9e4f8e63f6028b8c954aeb1ee2dd29d621c2bf27
GDR! <gdr@go2.pl>
2014-09-28 19:38:02 +0200

    Changed the defunct UbuntuONE with Github
    When checking for connectivity, the program was trying an Ubuntu One
    URL. Ubuntu One is dead.

--- src/com/sputnik/wispr/logger/WebLogger.java
+++ src/com/sputnik/wispr/logger/WebLogger.java
@@ -3,7 +3,7 @@ package com.sputnik.wispr.logger;
 public interface WebLogger {
 	public static final String CONNECTED = "CONNECTED";
 
-	public static final String BLOCKED_URL = "http://ubuntuone.com/p/F6b/";
+	public static final String BLOCKED_URL = "https://raw.githubusercontent.com/gjedeer/androidwisprclient/master/connected.txt";
 
 	public LoggerResult login(String user, String password);
 }
