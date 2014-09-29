a839b9e57805e1648d529c5e124ff503383a05c1
GDR! <gdr@go2.pl>
2014-09-29 20:16:19 +0200

    Removed MCC-dependent banners

--- src/com/oakley/fon/WISPrLoggerService.java
+++ src/com/oakley/fon/WISPrLoggerService.java
@@ -29,7 +29,6 @@ import android.util.Log;
 import com.oakley.fon.logger.BTFonLogger;
 import com.oakley.fon.logger.LivedoorLogger;
 import com.oakley.fon.logger.LoggerResult;
-import com.oakley.fon.logger.NetiaLogger;
 import com.oakley.fon.logger.NeufLogger;
 import com.oakley.fon.logger.WISPrLogger;
 import com.oakley.fon.logger.WebLogger;
@@ -59,8 +58,6 @@ public class WISPrLoggerService extends IntentService {
 			logger = new BTFonLogger();
 		} else if (FONUtils.isLivedoor(ssid, bssid)) {
 			logger = new LivedoorLogger();
-        } else if (FONUtils.isNetiaFonera(ssid, bssid)) {
-            logger = new NetiaLogger();
 		} else {
 			logger = new WISPrLogger();
 		}
